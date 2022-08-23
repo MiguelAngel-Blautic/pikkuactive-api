bd19c75deff233291f042cbfb7bb466950f557a9
Miguel Angel <miguelangel.callejo@blautic.com>
2022-08-23 09:06:15 +0200

    Arreglado el test

--- app/api/api_v1/endpoints/test.py
+++ app/api/api_v1/endpoints/test.py
@@ -9,11 +9,4 @@ router = APIRouter()
 def test(
         id: int,
 ) -> Any:
-    engine = db.create_engine('mariadb+mariadbconnector://root:$Sqnon2022!@82.223.19.236:3306/ziven-active')
-    connection = engine.connect()
-    metadata = db.MetaData()
-    user = db.Table('tblUser', metadata, autoload=True, autoload_with=engine)
-    query = db.select([user.columns.fldSFullName]).where(user.columns.id == id)
-    res = connection.execute(query).first()
-    print(res[0])
-    return res[0]
+    return id
