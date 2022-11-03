5040bafc41153e049f1edd59f5ad0d67522b6510
miguelangel <miguelangel.callejo@blautic.com>
2022-11-03 12:51:42 +0100

    crear usuarios con cualquier rol

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -126,7 +126,6 @@ def create_user_open(
             status_code=400,
             detail="The user with this username already exists in the system.",
         )
-    user_in.fkRol = 1
     user = crud.user.create(db, obj_in=user_in)
     if settings.EMAILS_ENABLED and user_in.fldSEmail:
         send_new_account_email(
