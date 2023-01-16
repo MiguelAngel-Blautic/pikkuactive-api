b7f549dadc3924f8d3d7297feaf504fa3fe11a7a
miguelangel <miguelangel.callejo@blautic.com>
2023-01-16 12:49:01 +0100

    Entrenamiento para modelos de 4 sensores

--- app/api/api_v1/endpoints/nn_ecg.py
+++ app/api/api_v1/endpoints/nn_ecg.py
@@ -13,16 +13,33 @@ from sklearn.preprocessing import LabelEncoder
 from app.models import tbl_version
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.optimizers import Adadelta
-
+import os
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import uuid
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPool2D
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from app.models import tbl_version
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.optimizers import Adadelta
 # CONFIG
 DATA_FREQ = 20
 SENS_NUMBER = 1
 
-
-def create_model(model,output_size=2):
+def create_model(model, output_size=2):
     n_devices = len(model.devices)
     samples = model.fldNDuration * DATA_FREQ
-    input_shape = (n_devices*samples,)
+    # input_shape = (n_devices*samples,)
+    columns = 0
+    for d in model.devices:
+        columns += d.fldNSensores
+    # samples = model.fldNDuration * DATA_FREQ
+    input_shape = (samples * columns,)
 
     modelEmg = Sequential()
     modelEmg.add(Dense(20, input_shape=input_shape, activation='sigmoid'))
@@ -37,7 +54,7 @@ def create_model(model,output_size=2):
     return model_uuid
 
 
-def train_model(model, df, version_last, max_value = 1):
+def train_model(model, df, version_last, max_value=1):
     url = ''
     if version_last is None or version_last.url is None or not os.path.exists(version_last.url):
         url = create_model(model, len(model.movements))
@@ -51,8 +68,7 @@ def train_model(model, df, version_last, max_value = 1):
     tf_model.compile(optimizer=Adam(learning_rate=0.0035), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     tf_model.fit(X_train_Emg, y_train, batch_size=32, shuffle=True, epochs=num_epoch,
-                           validation_data=(X_test_Emg, y_test), verbose=1)
-
+                 validation_data=(X_test_Emg, y_test), verbose=1)
 
     tf_model.save(url)
     evaluation = tf_model.evaluate(X_test_Emg, y_test)
@@ -66,7 +82,7 @@ def train_model(model, df, version_last, max_value = 1):
     return version
 
 
-def split_normalize_data(model, df , max_value):
+def split_normalize_data(model, df, max_value):
     label_enc = LabelEncoder()
     label_enc.fit([x.fldSLabel for x in model.movements])
 
@@ -93,17 +109,24 @@ def generate_columns_index(end):
 
 def data_adapter(model, captures):
     n_devices = len(model.devices)
+
     samples = model.fldNDuration * DATA_FREQ
 
-    columns_list = generate_columns_index(n_devices*samples)
+    columns = 0
+    for d in model.devices:
+        columns += d.fldNSensores
+    columns_list = generate_columns_index(samples * columns)
 
     df = pd.DataFrame(columns=columns_list)
     print(df)
     labels = []
+
     for capture in captures:
+
         only_data = []
-        my_range = len(capture.ecg)
-        if my_range == samples * n_devices:
+        my_range = len(capture.ecg)*columns
+
+        if my_range == samples * columns:
             for i_data in range(0, len(capture.ecg), len(model.devices)):
                 data_x = []
                 for i in range(len(model.devices)):
@@ -113,10 +136,17 @@ def data_adapter(model, captures):
 
                 list_sorted = []
                 for data in sorted_data:
-                    list_sorted.append(data.fldFData)
+                    n_sens = data.device.fldNSensores
+                    if n_sens > 0:
+                        list_sorted.append(data.fldFData0)
+                    if n_sens > 1:
+                        list_sorted.append(data.fldFData1)
+                    if n_sens > 2:
+                        list_sorted.append(data.fldFData2)
+                    if n_sens > 3:
+                        list_sorted.append(data.fldFData3)
 
                 only_data.append(list_sorted)
-
 
                 # print(i_data)
 
@@ -124,7 +154,7 @@ def data_adapter(model, captures):
             np_data = np.resize(only_data, (1, len(only_data) * len(only_data[0])))
             df_length = len(df)
             df.loc[df_length] = np_data[0].tolist()
-    pd.set_option('display.max_columns', n_devices * samples)
+    pd.set_option('display.max_columns', columns * samples)
     df['fldSLabel'] = labels
     # print(df.shape)
     # print(df)
@@ -134,3 +164,15 @@ def data_adapter(model, captures):
 
 def saveModel():
     pass
+
+
+
+
+
+
+
+
+
+
+
+
