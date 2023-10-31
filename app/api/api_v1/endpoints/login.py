522ddcdb30d9e1970e0b2f4a949943fcef56e501
miguelangel <miguelangel.callejo@blautic.com>
2023-10-31 11:51:24 +0100

    login1

--- app/api/api_v1/endpoints/login.py
+++ app/api/api_v1/endpoints/login.py
@@ -19,6 +19,31 @@ from app.utils import (
 router = APIRouter()
 
 
+@router.post("/login/access-token", response_model=schemas.Token)
+def login_access_token(
+    db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
+) -> Any:
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = crud.user.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    elif not crud.user.fldBActive(user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {
+        "access_token": security.create_access_token(
+            user.id, expires_delta=access_token_expires, it_expire=False
+        ),
+        "token_type": "bearer",
+        "rol": user.fkRol,
+        "id": user.id,
+    }
+
+
 @router.post("/login/test-token", response_model=schemas.User)
 def test_token(current_user: models.tbl_user = Depends(deps.get_current_user)) -> Any:
     """
