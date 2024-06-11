e5b2e3a9dabccab1e36d494ef061285987dded4e
miguelangel <miguelangel.callejo@blautic.com>
2024-06-11 10:48:34 +0200

    Commits cada 1000 datos

--- app/api/api_v1/endpoints/ejercicios.py
+++ app/api/api_v1/endpoints/ejercicios.py
@@ -3,6 +3,7 @@ from typing import Any, List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette.background import BackgroundTasks
 
 from app import models, schemas
 from app.api import deps
@@ -17,36 +18,73 @@ router = APIRouter()
 def add_sesion(
         *,
         sesion: Sesion,
-        db: Session = Depends(deps.get_db)
+        db: Session = Depends(deps.get_db),
+        background_tasks: BackgroundTasks
 ) -> Any:
-    new_sesion = tbl_sesion(fldDInicio = sesion.inicio,
-                            fldNEdad = sesion.edad,
-                            fldFAltura = sesion.altura,
-                            fldFPeso = sesion.peso,
-                            fldNSexo = sesion.sexo)
+    print("INICIADO")
+    background_tasks.add_task(completarSesion, sesion, db)
+    return json.loads('{"msg": "OK"}')
+
+
+def completarSesion(sesion: Sesion, db: Session):
+    new_sesion = tbl_sesion(fldDInicio=sesion.inicio,
+                            fldNEdad=sesion.edad,
+                            fldFAltura=sesion.altura,
+                            fldFPeso=sesion.peso,
+                            fldNSexo=sesion.sexo)
     db.add(new_sesion)
     db.commit()
     db.refresh(new_sesion)
+    id = new_sesion.id
+    print("SESION")
+    i = 0
+    j = 0
     for d in sesion.datos:
         new_dato = tbl_dato(fkSensor = d.sensor,
-                            fkSesion = new_sesion.id,
+                            fkSesion = id,
                             fldDTime = d.hora,
                             fldFValor = d.valor)
         db.add(new_dato)
+        i = i + 1
+        j = j + 1
+        if(j >= 1000):
+            j = 0
+            print(str(i)+"/"+str(len(sesion.datos)))
+            db.commit()
+    print("DATOS")
+    i = 0
+    j = 0
     for e in sesion.etiquetas:
         new_marca = tbl_marcas(fldNTipo = 2,
                                fldNValor = e.valor,
                                fldDTime = e.hora,
-                               fkSesion = new_sesion.id)
+                               fkSesion = id)
         db.add(new_marca)
+        i = i + 1
+        j = j + 1
+        if(j >= 1000):
+            j = 0
+            print(str(i)+"/"+str(len(sesion.etiquetas)))
+            db.commit()
+    print("MARCAS")
+    i = 0
+    j = 0
     for e in sesion.fases:
         new_marca = tbl_marcas(fldNTipo = 1,
                                fldNValor = e.valor,
                                fldDTime = e.hora,
-                               fkSesion = new_sesion.id)
+                               fkSesion = id)
         db.add(new_marca)
+        i = i + 1
+        j = j + 1
+        if(j >= 1000):
+            j = 0
+            print(str(i)+"/"+str(len(sesion.fases)))
+            db.commit()
+    print("FASES")
     db.commit()
-    return json.loads('{"id": '+str(new_sesion.id)+'}')
+    print("COMMITED")
+    return json.loads('{"id": '+str(id)+'}')
 
 
 @router.get("/")
