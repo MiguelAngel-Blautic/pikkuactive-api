c469d39f36dfcbfafdbc5daef2be81c437164fe8
miguelangel <miguelangel.callejo@blautic.com>
2024-10-04 08:47:17 +0200

    Añadir etiquetas adicionales

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import models, grupoNegativo, login, users, utils, position, captures
+from app.api.api_v1.endpoints import models, grupoNegativo, login, users, utils, position, captures, movements
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -10,3 +10,4 @@ api_router.include_router(captures.router, prefix="/capture", tags=["captures"])
 api_router.include_router(position.router, prefix="/position", tags=["positions"])
 api_router.include_router(grupoNegativo.router, prefix="/grupo", tags=["grupo"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(movements.router, prefix="/movements", tags=["movements"])
---
+++ app/api/api_v1/endpoints/movements.py
@@ -0,0 +1,43 @@
+from typing import Any, List
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+from app.models import tbl_movement
+from app.models.tbl_position import tbl_position
+from app.schemas import MovementCreate
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[schemas.Movement])
+def read_movements(
+    id: int,
+    db: Session = Depends(deps.get_db),
+    current_user: models.tbl_user = Depends(deps.get_current_active_user),
+) -> Any:
+    """
+    Retrieve positions.
+    """
+    movements = db.query(tbl_movement).filter(tbl_movement.fkOwner == id).all()
+    db.close()
+    return movements
+
+
+@router.post("/", response_model=schemas.Movement)
+def create_movement(
+    id: int,
+    nombre: str,
+    db: Session = Depends(deps.get_db),
+    current_user: models.tbl_user = Depends(deps.get_current_active_user),
+) -> Any:
+    """
+    Retrieve positions.
+    """
+    movement_incorrect = MovementCreate(fldSLabel=nombre, fldSDescription=nombre)
+    crud.movement.create_with_owner(db=db, obj_in=movement_incorrect, fkOwner=id)
+    db.refresh(movement_incorrect)
+    db.close()
+    return movement_incorrect
